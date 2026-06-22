79c084f8a22f5254b2061847196a8390af92dab3
Asaduj Zaman <asadujzaman.cst@gmail.com>
2026-06-22 23:08:13 +0600

    first commit

---
+++ python_assignment_1/area_calculator.py
@@ -0,0 +1,10 @@
+#python assigmnmet_1 area calculator
+lenght = float(input("Entet the lenght :"))
+width = float(input("Enter width :"))
+'''
+math formula for area of rectangle is
+area = lenght * width
+
+'''
+area = lenght * width
+print(area)
